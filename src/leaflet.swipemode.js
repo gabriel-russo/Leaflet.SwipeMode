57e6143a618f71b560b302e550235be066cc9969
J.Lânio <lanio.morais@gmail.com>
2023-09-05 09:41:34 -0400

    Input range Button substituído por um fake Button
    Ação necessária em função de um bug ocorrido quando a tela/screen é modificada.

--- src/leaflet.swipemode.js
+++ src/leaflet.swipemode.js
@@ -1,18 +1,22 @@
-import * as L from 'leaflet';
-import './leaflet.swipemode.css';
-import { findLayerById, getRangeEvent } from './swipemode.utils';
-import { createContainer, createExitBar, createLayerPicker } from './swipemode.dom';
+import * as L from "leaflet";
+import "./leaflet.swipemode.css";
+import { findLayerById, getRangeEvent } from "./swipemode.utils";
+import {
+  createContainer,
+  createExitBar,
+  createLayerPicker,
+} from "./swipemode.dom";
 
 L.Control.SwipeMode = L.Control.extend({
   options: {
-    position: 'topleft',
+    position: "topleft",
     thumbSize: 42,
     padding: 0,
     noControl: false,
     text: {
-      title: 'Enable Swipe Mode',
-      leftLayerSelector: 'Left Layer',
-      rightLayerSelector: 'Right Layer',
+      title: "Enable Swipe Mode",
+      leftLayerSelector: "Left Layer",
+      rightLayerSelector: "Right Layer",
     },
   },
 
@@ -41,29 +45,28 @@ L.Control.SwipeMode = L.Control.extend({
 
   onAdd(map) {
     if (this.options.noControl) {
-      return L.DomUtil.create('div', 'leaflet-bar'); // Leaflet needs a DOM element returned
+      return L.DomUtil.create("div", "leaflet-bar"); // Leaflet needs a DOM element returned
     }
 
     if (this.options.button) {
-      L.DomEvent.on(this.options.button, 'click', this.toggle, this);
-      return L.DomUtil.create('div', 'leaflet-bar'); // Leaflet needs a DOM element returned
+      L.DomEvent.on(this.options.button, "click", this.toggle, this);
+      return L.DomUtil.create("div", "leaflet-bar"); // Leaflet needs a DOM element returned
     }
 
-    let className = 'leaflet-control-sm';
+    let className = "leaflet-control-sm";
 
-    const container = L.DomUtil.create('div', `leaflet-bar`);
+    const container = L.DomUtil.create("div", `leaflet-bar`);
 
-    let link = L.DomUtil.create('a', `${className}-button`, container);
+    let link = L.DomUtil.create("a", `${className}-button`, container);
 
-    link.href = '#';
+    link.href = "#";
 
     link.title = this.options.text.title;
 
-    L.DomEvent
-      .addListener(link, 'click', L.DomEvent.stopPropagation)
-      .addListener(link, 'click', L.DomEvent.preventDefault)
-      .addListener(link, 'click', this._openLayerSelector, this)
-      .addListener(link, 'click', this.toggle, this);
+    L.DomEvent.addListener(link, "click", L.DomEvent.stopPropagation)
+      .addListener(link, "click", L.DomEvent.preventDefault)
+      .addListener(link, "click", this._openLayerSelector, this)
+      .addListener(link, "click", this.toggle, this);
 
     return container;
   },
@@ -76,11 +79,11 @@ L.Control.SwipeMode = L.Control.extend({
     if (!this.enabled()) {
       this._isSwipeModeActive = true;
       this._enable();
-      this._map.fire('swipemode:start');
+      this._map.fire("swipemode:start");
     } else {
       this._isSwipeModeActive = false;
       this._disable();
-      this._map.fire('swipemode:stop');
+      this._map.fire("swipemode:stop");
     }
   },
 
@@ -104,21 +107,40 @@ L.Control.SwipeMode = L.Control.extend({
 
   _getDividerPosition() {
     let rangeValue = this._range.value;
-    let offset = (0.5 - rangeValue) * (2 * this.options.padding + this.options.thumbSize);
+    let offset =
+      (0.5 - rangeValue) * (2 * this.options.padding + this.options.thumbSize);
     return this._map.getSize().x * rangeValue + offset;
   },
 
   _enable() {
-    this._swipeModeContainer = L.DomUtil.create('div', 'leaflet-sm', this._map.getContainer());
+    this._swipeModeContainer = L.DomUtil.create(
+      "div",
+      "leaflet-sm",
+      this._map.getContainer()
+    );
 
-    this._divider = L.DomUtil.create('div', 'leaflet-sm-divider', this._swipeModeContainer);
+    this._divider = L.DomUtil.create(
+      "div",
+      "leaflet-sm-divider",
+      this._swipeModeContainer
+    );
 
-    this._range = L.DomUtil.create('input', 'leaflet-sm-range', this._swipeModeContainer);
+    this._range = L.DomUtil.create(
+      "input",
+      "leaflet-sm-range",
+      this._swipeModeContainer
+    );
 
-    this._range.type = 'range';
+    this._button = L.DomUtil.create(
+      "div",
+      "leaflet-sm-button",
+      this._swipeModeContainer
+    );
+
+    this._range.type = "range";
     this._range.min = 0;
     this._range.max = 1;
-    this._range.step = 'any';
+    this._range.step = "any";
     this._range.value = 0.5;
     this._range.style.paddingLeft = `${this.options.padding}px`;
     this._range.style.paddingRight = `${this.options.padding}px`;
@@ -133,12 +155,12 @@ L.Control.SwipeMode = L.Control.extend({
 
     if (this._leftLayer) {
       this._leftLayer.setZIndex(this._sourceLeftLayerZindex);
-      this._leftLayer.getContainer().style.clip = '';
+      this._leftLayer.getContainer().style.clip = "";
     }
 
     if (this._rightLayer) {
       this._rightLayer.setZIndex(this._sourceRightLayerZindex);
-      this._rightLayer.getContainer().style.clip = '';
+      this._rightLayer.getContainer().style.clip = "";
     }
 
     this._removeEvents();
@@ -153,10 +175,12 @@ L.Control.SwipeMode = L.Control.extend({
     let dividerX = this._getDividerPosition();
 
     this._divider.style.left = `${dividerX}px`;
-    this.fire('swipemode:dividermove', { x: dividerX });
+    this._button.style.left = `${dividerX}px`;
 
-    let clipLeft = `rect(${[nw.y, clipX, se.y, nw.x].join('px,')}px)`;
-    let clipRight = `rect(${[nw.y, se.x, se.y, clipX].join('px,')}px)`;
+    this.fire("swipemode:dividermove", { x: dividerX });
+
+    let clipLeft = `rect(${[nw.y, clipX, se.y, nw.x].join("px,")}px)`;
+    let clipRight = `rect(${[nw.y, se.x, se.y, clipX].join("px,")}px)`;
 
     if (this._leftLayer) {
       this._leftLayer.getContainer().style.clip = clipLeft;
@@ -191,18 +215,18 @@ L.Control.SwipeMode = L.Control.extend({
       if (prevLeft) {
         prevLeft.setZIndex(this._sourceLeftLayerZindex);
       }
-      this._map.fire('swipemode:newlayer');
-      this._map.fire('swipemode:leftlayerremove', { layer: prevLeft });
-      this._map.fire('swipemode:leftlayeradd', { layer: this._leftLayer });
+      this._map.fire("swipemode:newlayer");
+      this._map.fire("swipemode:leftlayerremove", { layer: prevLeft });
+      this._map.fire("swipemode:leftlayeradd", { layer: this._leftLayer });
     }
 
     if (prevRight !== this._rightLayer) {
       if (prevRight) {
         prevRight.setZIndex(this._sourceRightLayerZindex);
       }
-      this._map.fire('swipemode:newlayer');
-      this.fire('swipemode:rightlayerremove', { layer: prevRight });
-      this.fire('swipemode:rightlayeradd', { layer: this._rightLayer });
+      this._map.fire("swipemode:newlayer");
+      this.fire("swipemode:rightlayerremove", { layer: prevRight });
+      this.fire("swipemode:rightlayeradd", { layer: this._rightLayer });
     }
 
     this._updateClip();
@@ -224,61 +248,95 @@ L.Control.SwipeMode = L.Control.extend({
   _addEvents() {
     if (!this._map || !this._range) return;
 
-    this._map.on('move', this._updateClip, this);
+    this._map.on("move", this._updateClip, this);
 
-    this._map.on('swipemode:newlayer', this._updateLayers, this);
+    this._map.on("swipemode:newlayer", this._updateLayers, this);
 
-    L.DomEvent.on(this._range, getRangeEvent(this._range), this._updateClip, this);
+    L.DomEvent.on(
+      this._range,
+      getRangeEvent(this._range),
+      this._updateClip,
+      this
+    );
 
-    L.DomEvent.on(this._range, 'touchend', this._enableMapDrag, this)
-      .on(this._range, 'mouseup', this._enableMapDrag, this);
+    L.DomEvent.on(this._range, "touchend", this._enableMapDrag, this).on(
+      this._range,
+      "mouseup",
+      this._enableMapDrag,
+      this
+    );
 
-    L.DomEvent.on(this._range, 'touchstart', this._cancelMapDrag, this)
-      .on(this._range, 'mousedown', this._cancelMapDrag, this);
+    L.DomEvent.on(this._range, "touchstart", this._cancelMapDrag, this).on(
+      this._range,
+      "mousedown",
+      this._cancelMapDrag,
+      this
+    );
   },
 
   _removeEvents() {
     if (this._range) {
-      L.DomEvent.off(this._range, getRangeEvent(this._range), this._updateClip, this)
-        .off(this._range, 'touchend', this._enableMapDrag, this)
-        .off(this._range, 'mouseup', this._enableMapDrag, this)
-        .off(this._range, 'mousedown', this._cancelMapDrag, this)
-        .off(this._range, 'touchstart', this._cancelMapDrag, this);
+      L.DomEvent.off(
+        this._range,
+        getRangeEvent(this._range),
+        this._updateClip,
+        this
+      )
+        .off(this._range, "touchend", this._enableMapDrag, this)
+        .off(this._range, "mouseup", this._enableMapDrag, this)
+        .off(this._range, "mousedown", this._cancelMapDrag, this)
+        .off(this._range, "touchstart", this._cancelMapDrag, this);
     }
     if (this._map) {
-      this._map.off('swipemode:newlayer', this._updateLayers, this);
-      this._map.off('move', this._updateClip, this);
+      this._map.off("swipemode:newlayer", this._updateLayers, this);
+      this._map.off("move", this._updateClip, this);
     }
   },
 
   _openLayerSelector() {
-    L.DomUtil.addClass(this._container, 'leaflet-control-sm-layer-selector-open');
+    L.DomUtil.addClass(
+      this._container,
+      "leaflet-control-sm-layer-selector-open"
+    );
 
-    this._layerPickerContainer = createContainer('leaflet-control-sm-layer-selector-container', this._container);
+    this._layerPickerContainer = createContainer(
+      "leaflet-control-sm-layer-selector-container",
+      this._container
+    );
 
     createExitBar(this._layerPickerContainer, this._closeLayerSelector, this);
 
-    const pickerContainer = createContainer('leaflet-control-sm-layer-picker-container', this._layerPickerContainer);
+    const pickerContainer = createContainer(
+      "leaflet-control-sm-layer-picker-container",
+      this._layerPickerContainer
+    );
 
     createLayerPicker(
       this.options.text.leftLayerSelector,
       pickerContainer,
-      (e) => { this.setLeftLayer(findLayerById(this._map, e.target.value)); },
-      'left',
-      this,
+      (e) => {
+        this.setLeftLayer(findLayerById(this._map, e.target.value));
+      },
+      "left",
+      this
     );
 
     createLayerPicker(
       this.options.text.rightLayerSelector,
       pickerContainer,
-      (e) => { this.setRightLayer(findLayerById(this._map, e.target.value)); },
-      'right',
-      this,
+      (e) => {
+        this.setRightLayer(findLayerById(this._map, e.target.value));
+      },
+      "right",
+      this
     );
   },
 
   _closeLayerSelector() {
-    L.DomUtil.removeClass(this._container, 'leaflet-control-sm-layer-selector-open');
+    L.DomUtil.removeClass(
+      this._container,
+      "leaflet-control-sm-layer-selector-open"
+    );
     if (this._layerPickerContainer) {
       L.DomUtil.empty(this._layerPickerContainer);
     }
@@ -286,4 +344,5 @@ L.Control.SwipeMode = L.Control.extend({
   },
 });
 
-L.Control.swipeMode = (leftLayer, rightLayer, options) => new L.Control.SwipeMode(leftLayer, rightLayer, options);
+L.Control.swipeMode = (leftLayer, rightLayer, options) =>
+  new L.Control.SwipeMode(leftLayer, rightLayer, options);
